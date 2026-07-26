77c26a370d740455680f458bdd2ca87cf6c6a4b7
negocil-nego <negocil.nego@gmail.com>
2026-07-26 12:24:01 +0100

    feat: create data-table

---
+++ src/lib/components/core/data-table/ui/DataTableDragHandle.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { HugeiconsIcon } from "@hugeicons/svelte";
+    import { DragDropVerticalIcon } from "@hugeicons/core-free-icons";
+    import type { Attachment } from "svelte/attachments";
+    let { attach }: { attach?: Attachment } = $props();
+</script>
+
+<Button
+    {@attach attach}
+    variant="ghost"
+    size="icon"
+    class="text-muted-foreground size-7 hover:bg-transparent"
+>
+    <HugeiconsIcon
+        icon={DragDropVerticalIcon}
+        size={12}
+        class="text-muted-foreground"
+    />
+    <span class="sr-only">Drag to reorder</span>
+</Button>
